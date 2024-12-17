ee3cc13b510877b3b08b7b60273ec8e6ba1a25ad
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2024-12-17 15:55:53 +0000

    Added header, total

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print("\n")
 
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
@@ -32,11 +32,46 @@ for filepath in filepaths:
     pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=50, h=8, txt=f"Date. {day}-{month}-{year}", align="L", ln=1)
 
+    # Read data from dataframe
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
+    # get column headings
+    columns = df.columns
 
+    formatted_columns = [col.replace("_", " ").title() for col in columns]
 
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=formatted_columns[0], border=1,)
+    pdf.cell(w=70, h=8, txt=formatted_columns[1], border=1,)
+    pdf.cell(w=35, h=8, txt=formatted_columns[2], border=1,)
+    pdf.cell(w=30, h=8, txt=f"{formatted_columns[3]} (£)", border=1,)
+    pdf.cell(w=25, h=8, txt=f"{formatted_columns[4]} (£)", border=1, ln=1)
 
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=35, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=25, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+    total = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=35, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=25, h=8, txt=str(total), border=1, ln=1)
 
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(255, 80, 80)
+    pdf.cell(w=30, h=8, txt="")
+    pdf.cell(w=70, h=8, txt="")
+    pdf.cell(w=30, h=8, txt="")
+    pdf.cell(w=35, h=8, align="C", txt=f"The total price is:")
+    pdf.cell(w=25, h=8, txt=f"£{str(total)}")
 
     pdf.output(f"invoices/PDFs/{filename}.pdf")
