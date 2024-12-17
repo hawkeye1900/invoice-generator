29320a180c61b1f693145194166f4c1eba4d9357
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2024-12-17 14:16:29 +0000

    Invoice date added to pdf

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 import pandas as pd
 import glob
-
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 print(filepaths)
+print("\n")
 
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+
+    # extracts the filename, minus extension and any path to file, such as
+    # directories
+    filename = Path(filepath).stem
+    invoice_nr, invoice_date = filename.split("-")
+
+    # Add invoice number as title
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", align="L", ln=1)
+
+    # Adding date to invoice, extract date from filename and split
+    invoice_date = invoice_date.split(".")
+    invoice_date.reverse()
+
+    # destructure to individual date elements, ie day, month, year
+    day, month, year = invoice_date
+
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Date. {day}-{month}-{year}", align="L", ln=1)
+
+
+
+
+
+
+
+
+    pdf.output(f"invoices/PDFs/{filename}.pdf")
